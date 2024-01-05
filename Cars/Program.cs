0a634a889c93ffa147e9bc9bafcd324828a3dba1
Jakob spiel <jako5457@gmail.com>
2024-01-05 10:55:21 +0100

    Tilføjet csv fil og import-metoder

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cars;
+
+var cars = ProcessFile("fuel.csv");
+
+foreach (var car in cars)
+{
+    Console.WriteLine(car.Name);
+}
+
+#region Methods
+
+List<Car> ProcessFile(string path)
+{
+    return File.ReadAllLines(path)
+               .Skip(1)
+               .Where(l => l.Length > 1)
+               .Select(Car.ParseFromCsv)
+               .ToList();
+}
+
+#endregion
