bae869cd6fa53f6413b0e3ca6929922e1aeb7970
Jakob spiel <jako5457@gmail.com>
2024-01-05 14:21:27 +0100

    Rettet kode

---
+++ Cars/CarComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    internal class CarComparer : IEqualityComparer<Car>
+    {
+
+        public bool Equals(Car x, Car y)
+        {
+            if (string.Equals(x.Name, y.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int GetHashCode(Car obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+
+    }
+}
--- Cars/Program.cs
+++ Cars/Program.cs
@@ -2,36 +2,11 @@
 
 var cars = ProcessFile("fuel.csv");
 
-var query =
-    from car in cars
-    where car.Manufacturer == "BMW" && car.Year == 2016
-    orderby car.Combined descending, car.Name ascending
-    select car;
+var result = cars.Any(c => c.Manufacturer == "Ford");
+//var result = cars.All(c => c.Manufacturer == "Ford");
+//var result = cars.Contains(new Car { Name = "Prius Eco" }, new CarComparer());
 
-var query2 =
-    cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
-    .OrderByDescending(c => c.Combined)
-    .ThenBy(c => c.Name)
-    .Select(c => c);
-
-foreach (var car in query2.Take(10))
-{
-    Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
-}
-
-#region FIRST
-// First
-var top = cars
-   .Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
-   .OrderByDescending(c => c.Combined)
-   .ThenBy(c => c.Name)
-   .Select(c => c)
-   .First();
-//.First(c => c.Manufacturer == "BMW" && c.Year == 2016);
-//.FirstOrDefault(c => c.Manufacturer == "abc" && c.Year == 2016);
-
-Console.WriteLine($"\nFirst Car: {top?.Name ?? "No Car found"}");
-#endregion
+Console.WriteLine(result);
 
 #region Methods
 
