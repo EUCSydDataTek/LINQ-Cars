e7f2543ae1e4626f10b3b3894ed1c9d29db0a868
Jakob spiel <jako5457@gmail.com>
2024-01-05 14:27:17 +0100

    Rettet kode

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -2,11 +2,28 @@
 
 var cars = ProcessFile("fuel.csv");
 
-var result = cars.Any(c => c.Manufacturer == "Ford");
-//var result = cars.All(c => c.Manufacturer == "Ford");
-//var result = cars.Contains(new Car { Name = "Prius Eco" }, new CarComparer());
+var query =
+    from car in cars
+    where car.Manufacturer == "BMW" && car.Year == 2016
+    orderby car.Combined descending, car.Name ascending
+    select car;
+//select new
+//{
+//    car.Manufacturer,
+//    car.Name,
+//    car.Combined
+//};
 
-Console.WriteLine(result);
+//var query = cars
+//    .Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+//    .OrderByDescending(c => c.Combined)
+//    .ThenBy(c => c.Name)
+//    .Select(c => new
+//    {
+//        c.Manufacturer,
+//        c.Name,
+//        c.Combined
+//    });
 
 #region Methods
 
@@ -15,7 +32,7 @@ List<Car> ProcessFile(string path)
     return File.ReadAllLines(path)
                .Skip(1)
                .Where(l => l.Length > 1)
-               .Select(Car.ParseFromCsv)
+               .ToCar() // <- Extension from CarExtensions
                .ToList();
 }
 
