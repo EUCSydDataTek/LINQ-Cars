459bdb0f73fa37920c5b90264bdc152e06ff7c13
Jakob spiel <jako5457@gmail.com>
2024-01-15 13:21:55 +0100

    tilføjet eksempel

---
+++ Cars/CarStatistics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class CarStatistics
+    {
+        public CarStatistics()
+        {
+            Max = Int32.MinValue;
+            Min = Int32.MaxValue;
+        }
+
+        public CarStatistics Accumulate(Car car)
+        {
+            Count++;
+            Total += car.Combined;
+            Max = Math.Max(Max, car.Combined);
+            Min = Math.Min(Min, car.Combined);
+            return this;
+        }
+
+        public CarStatistics Compute()
+        {
+            double trueAverage = Total / Count;
+            Average = Convert.ToInt32(Math.Round(trueAverage, 0));
+            return this;
+        }
+
+        public int Max { get; set; }
+        public int Min { get; set; }
+        public int Total { get; set; }
+        public int Count { get; set; }
+        public int Average { get; set; }
+    }
+}
